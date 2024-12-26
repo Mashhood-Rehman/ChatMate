4651faa1cd289baae3890ad81ae1a8b9d3866dd3
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2024-12-26 09:56:11 -0800

    frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 dotenv.config();
 const port = process.env.PORT;
 const authRoutes = require("./routes/auth.route");
@@ -9,6 +10,7 @@ const connectDB = require("./lib/db");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.listen(port, () => {
