8c7b04daf79c51597b7a38a6def7eac68b8f8d38
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2025-01-04 08:52:08 -0800

    project-completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 dotenv.config();
 const port = process.env.PORT;
+const __dirname = path.resolve();
 const authRoutes = require("./routes/auth.route");
 const messageRoutes = require("./routes/message.route");
 const connectDB = require("./lib/db");
@@ -14,6 +16,14 @@ app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(port, () => {
   console.log("the port is running on");
   connectDB();
